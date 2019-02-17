1e12d7f41c7c9ac5cacf0e1cfe0d01899c34719e
Ariel <ariverac@uni.pe>
2019-02-17 18:31:22 -0500

    Agregar archivos de proyecto.

---
+++ TestApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using SharpUpdate;
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        private SharpUpdater updater;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = ProductName + "\n" + ProductVersion;
+            //FileInfo(@"X:\Folder\Subfolder\file.ext")
+            //updater = new SharpUpdater(Assembly.GetExecutingAssembly(), this, new Uri("https://raw.githubusercontent.com/henryxrl/SharpUpdate/master/project.xml"));
+            //updater = new SharpUpdater(Assembly.GetExecutingAssembly(), this, new Uri(new System.IO.FileInfo(@"..\..\..\project.xml").FullName));       // for local testing
+            updater = new SharpUpdater(Assembly.GetExecutingAssembly(), this, new Uri(new System.IO.FileInfo(@"..\..\..\project.xml").FullName));       // for local testing            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updater.DoUpdate();
+        }
+    }
+}
